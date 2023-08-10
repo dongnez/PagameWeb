97879c4c13070d962e3371c25fe2528aa6f1b9bf
Gnez <jimenezjimenezguillermo@gmail.com>
2023-08-10 15:13:26 +0200

    r shit

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -1,35 +1,35 @@
-import { HTMLAttributes } from 'react';
+import { HTMLAttributes } from "react";
 // import { FaGooglePlay, FaApple } from 'react-icons/fa'; // Importa los íconos de FaGooglePlay y FaApple desde react-icons/fa
 
-
 interface DownloadButtonProps {
-  device: 'android' | 'ios';
+  device: "android" | "ios";
 }
 
-const DownloadButton = ({ device, className, ...rest}:HTMLAttributes<HTMLButtonElement> & DownloadButtonProps) => {
-  const isAndroid = device === 'android';
+const DownloadButton = ({
+  device,
+  className,
+  ...rest
+}: HTMLAttributes<HTMLButtonElement> & DownloadButtonProps) => {
+  const isAndroid = device === "android";
 
   const handleClick = () => {
     if (isAndroid) {
-      console.log('ENLACE DE ANDROID');
+      console.log("ENLACE DE ANDROID");
     } else {
-      console.log('ENLACE DE APPLE');
+      console.log("ENLACE DE APPLE");
     }
   };
 
   return (
     <button
-        {...rest}
-        className={`bg-white rounded-full py-2 px-3 flex items-center text-black w-full justify-center transition-transform hover:scale-110 gap-1
+      {...rest}
+      className={`bg-white rounded-full py-2 px-3 flex items-center text-black w-full justify-center transition-transform hover:scale-110 gap-1
         ${className}`}
-      onClick={handleClick}
-    >
+      onClick={handleClick}>
       {/* {isAndroid ? <FaGooglePlay className="mr-2" size={20} /> : <FaApple className="mr-2" size={20}/>} */}
-      Download for <span className='uppercase'>{device}</span>
+      Download for <span className="uppercase">{device}</span>
     </button>
   );
 };
 
 export default DownloadButton;
-
-
