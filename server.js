061d99d17ecc4c910f0e044278978cc39886971d
Gnez <jimenezjimenezguillermo@gmail.com>
2023-08-10 14:34:43 +0200

    change vite env

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname,'dist')));
 
-const port = process.env.PORT || 3000
+const port = import.meta.env.PORT || 3000
 
 app.get("/api/news", (req, res) => {
   res.json({
