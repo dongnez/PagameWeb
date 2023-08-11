59630118f05d095218d6a5eae137d23a97cd6876
Gnez <jimenezjimenezguillermo@gmail.com>
2023-08-11 06:53:07 +0200

    update policy

--- src/modules/pages/PoliticaPrivacidad.tsx
+++ src/modules/pages/PoliticaPrivacidad.tsx
@@ -6,91 +6,144 @@ export class PoliticaPrivacidad extends Component {
     return (
       <div className="p-4 text-white max-w-6xl text-justify">
         <div className="flex">
-        <h2 className="text-4xl font-bold mb-4 mr-2 right">
-          Política de Privacidad de 
-        </h2>
-        <PagameTitle className="text-4xl"/>
+          <h2 className="text-4xl font-bold mb-4 mr-2 right">
+            Política de Privacidad de
+          </h2>
+          <PagameTitle className="text-4xl" />
         </div>
         <p className="mb-4 underline text-md">
           Última actualización: [27-07-2023]
         </p>
 
-        <p className="mb-4">
-          Bienvenido(a) a Pagame, una aplicación que facilita la división de
-          gastos en grupos. En Pagame, nos tomamos muy en serio la privacidad y
-          la seguridad de tus datos personales. Esta Política de Privacidad
-          tiene como objetivo proporcionarte información transparente sobre cómo
-          recopilamos, utilizamos, compartimos y protegemos tus datos. Al
-          utilizar nuestra aplicación, aceptas las prácticas descritas en esta
+        <h2 className="text-2xl font-bold mb-6">
+          Política de Privacidad de Pagame
+        </h2>
+
+        <p>
+          Bienvenido(a) a Pagame, una aplicación diseñada para simplificar la
+          gestión de gastos en grupos. En Pagame, valoramos y respetamos tu
+          privacidad. Esta Política de Privacidad explica cómo recopilamos,
+          utilizamos, compartimos y protegemos tus datos personales cuando
+          utilizas nuestra aplicación en las plataformas móviles iOS y Android.
+          Al acceder y utilizar Pagame, aceptas los términos descritos en esta
           política.
         </p>
 
-        <h3 className="text-xl font-bold mb-2">
-          1. Información que recopilamos:
+        <h3 className="text-xl font-semibold mt-6 mb-3">
+          1. Información que Recopilamos
         </h3>
-        <p className="mb-4">
-          Para brindarte una experiencia personalizada y funcional, recopilamos
-          la siguiente información:
-        </p>
-
-        <ul className="list-disc list-inside mb-4">
-          <li>
-            Información de registro: Al crear una cuenta en Pagame, solicitamos
-            información básica, como tu nombre de usuario, dirección de correo electrónico
-            y contraseña.
+        <ul className="list-disc list-inside">
+          <li className="mb-2">
+            a) Información de Registro: Cuando creas una cuenta en Pagame,
+            solicitamos información básica como tu nombre de usuario, dirección
+            de correo electrónico y contraseña.
           </li>
-          <li>
-            Información de transacciones: Al usar la aplicación para compartir
-            gastos, recopilamos información sobre las transacciones y los gastos
-            compartidos entre los miembros del grupo.
+          <li className="mb-2">
+            b) Información de Transacciones: Al utilizar la aplicación para
+            compartir gastos en grupos, recopilamos información sobre las
+            transacciones y los gastos compartidos entre los miembros del grupo.
           </li>
-          <li>
-            Información de contacto: Si decides invitar a otros a unirse a tu
-            grupo de Pagame, podemos recopilar información de contacto, como
-            nombres y direcciones de correo electrónico.
+          <li className="mb-2">
+            c) Información de Contacto: Si decides invitar a otros a unirse a tu
+            grupo en Pagame, podemos recopilar información de contacto,
+            incluyendo nombres y direcciones de correo electrónico.
           </li>
-          <li>
-            Información del dispositivo: Recopilamos datos técnicos, como la
-            dirección IP, el tipo de dispositivo, la versión del sistema
-            operativo y la configuración del idioma, para mejorar la calidad y
-            seguridad de nuestro servicio.
+          <li className="mb-2">
+            d) Información del Dispositivo: Recopilamos datos técnicos, como la
+            dirección IP, tipo de dispositivo, versión del sistema operativo y
+            configuración de idioma, con el propósito de mejorar la calidad y
+            seguridad de nuestros servicios.
           </li>
         </ul>
 
-        <h3 className="text-xl font-bold mb-2">2. Uso de la información:</h3>
-        <p className="mb-4">
+        <h3 className="text-xl font-semibold mt-6 mb-3">
+          2. Uso de la Información
+        </h3>
+        <p>
           Utilizamos la información recopilada para los siguientes propósitos:
         </p>
-
-        <ul className="list-disc list-inside mb-4">
-          <li>Proporcionar y mejorar nuestros servicios.</li>
-          <li>Administrar tu cuenta y brindar soporte al cliente.</li>
-          <li>
-            Facilitar la división de gastos y mantener un registro de las
-            transacciones.
+        <ul className="list-disc list-inside">
+          <li className="mb-2">
+            Proporcionar y mejorar nuestros servicios, incluyendo la
+            personalización de tu experiencia en la aplicación.
           </li>
-          <li>
-            Enviar notificaciones relevantes sobre tu actividad en Pagame.
+          <li className="mb-2">
+            Administrar tu cuenta y brindar soporte al cliente cuando sea
+            necesario.
           </li>
-          <li>
+          <li className="mb-2">
+            Facilitar la división de gastos y llevar un registro de las
+            transacciones dentro de los grupos.
+          </li>
+          <li className="mb-2">
+            Enviarte notificaciones pertinentes sobre tus actividades en Pagame.
+          </li>
+          <li className="mb-2">
             Enviar información y promociones relacionadas con nuestra
-            aplicación, siempre que hayas dado tu consentimiento previo.
+            aplicación, siempre y cuando hayas otorgado tu consentimiento previo
+            para recibir dichas comunicaciones.
           </li>
         </ul>
 
-        {/* Otras secciones de la política de privacidad */}
+        <h3 className="text-xl font-semibold mt-6 mb-3">
+          3. Compartir de la Información
+        </h3>
+        <p>
+          Nos comprometemos a proteger tu privacidad y no compartimos tus datos
+          personales con terceros sin tu consentimiento, excepto en los
+          siguientes casos:
+        </p>
+        <ul className="list-disc list-inside">
+          <li className="mb-2">
+            Con proveedores de servicios que trabajan en nuestro nombre y están
+            sujetos a acuerdos de confidencialidad.
+          </li>
+          <li className="mb-2">
+            Cuando sea necesario para cumplir con la ley, regulaciones o
+            solicitudes legales.
+          </li>
+          <li className="mb-2">
+            En caso de fusión, adquisición o venta de activos, donde la
+            información del usuario pueda ser transferida como parte de dicha
+            transacción.
+          </li>
+        </ul>
 
-        <p className="mb-4">
-          Si tienes preguntas o inquietudes sobre nuestra Política de
-          Privacidad, no dudes en contactarnos a través de [correo electrónico
-          de contacto].
+        <h3 className="text-xl font-semibold mt-6 mb-3">
+          4. Seguridad de los Datos
+        </h3>
+        <p>
+          Mantenemos medidas de seguridad técnicas y organizativas para proteger
+          tu información contra el acceso no autorizado, la pérdida y la
+          alteración. Sin embargo, es importante recordar que ninguna
+          transmisión de datos por Internet o método de almacenamiento
+          electrónico es completamente seguro.
         </p>
 
-        <p className="mb-4">
+        <h3 className="text-xl font-semibold mt-6 mb-3">5. Pagos Integrados</h3>
+        <p>
+          Pagame puede incluir funcionalidades de pagos integrados para
+          facilitar transacciones entre los miembros del grupo. Para ello,
+          utilizamos proveedores de servicios de pago confiables que cumplen con
+          estándares de seguridad de la industria.
+        </p>
+
+        <p className="mt-6">
+          Si tienes preguntas o inquietudes sobre esta Política de Privacidad,
+          contáctanos en:{" "}
+          <a href="mailto:pagame.me@proton.me">pagame.me@proton.me</a>.
+        </p>
+
+        <p className="mt-6">
           Al utilizar Pagame, aceptas los términos y condiciones de esta
-          Política de Privacidad. Gracias por confiar en nosotros para facilitar
-          tus finanzas en grupo de manera segura y confiable.
+          Política de Privacidad. Gracias por confiar en nosotros para gestionar
+          tus finanzas en grupo de manera segura y eficiente.
         </p>
+
+{/*         <p className="mt-6">
+          Fecha de entrada en vigor: [Fecha de entrada en vigor]
+        </p> */}
+
       </div>
     );
   }
